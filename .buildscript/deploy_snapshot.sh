0951d455899505d36d258c2135141578bd8d0a44
Christian Melchior <christian@ilios.dk>
2016-08-01 21:30:18 +0200

    Fix BinTray support (#1)
    Travis CI doesn't support Android 24 yet, so remove it for now.
    Also fixed other issues related to BinTray upload.

--- .buildscript/deploy_snapshot.sh
+++ .buildscript/deploy_snapshot.sh
@@ -5,7 +5,7 @@
 # Adapted from https://coderwall.com/p/9b_lfq and
 # http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
 
-SLUG="cmelchior/spanner"
+SLUG="cmelchior/realmfieldnameshelper"
 JDK="oraclejdk7"
 BRANCH="master"
 
